42aaf59a7c9a13e0178c7fd14f548e4841f34459
handsomeX <1258849260@qq.com>
2022-11-19 12:58:40 +0800

    feat: fsbuild - set root path

--- lib/src/build/buildfs.d.ts
+++ lib/src/build/buildfs.d.ts
@@ -4,8 +4,12 @@ declare type ASTNode = {
     children?: ASTNode[];
 };
 export declare class FSManager {
+    static root: string;
     static buildFS(): void;
+    static gen(input: string): void;
+    static setRootPath(input: string): string;
     static compile2AST(input: string): ASTNode;
+    static genStringTree(astNode: ASTNode, level?: number, str?: string): string;
     static ask2GenFile(astNode: ASTNode): void;
     static genFile(astNode: ASTNode, root?: string): void;
     static ask2Exit(): void;
